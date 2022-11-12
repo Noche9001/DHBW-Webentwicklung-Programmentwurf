aee73bdfff0d4eebc241652743d91913dd7eea39
Noche9001 <noah.dambacher@gmx.de>
2022-11-12 07:43:57 +0100

    Stand 12/11 -UI Überarbeitung -Raumname prüfung (-Scrollbar)
    TODO
    -Username Eingabe
    -Nachrichten an einzelne User

--- js/room.js
+++ js/room.js
@@ -1,19 +1,33 @@
 let roomId;
-const messageInput = document.getElementById("message")
+const canvas = document.getElementById("canvas");
+const messageInput = document.getElementById("message");
+const roomIdSpan = document.getElementById("roomId");
+const title = document.getElementById("title");
 let messages;
 
 window.onload = () => {
     const params = window.location.search;
     const urlParams = new URLSearchParams(params);
 
-    roomId = urlParams.get('roomId')
-    init();
+    roomId = urlParams.get('roomId');
+
+    if(roomId == null || roomId === "") {
+        window.location.href = "/Programmentwurf/";
+    }
+    else {
+        title.innerText = "Raum " + roomId;
+        init();
+    }
 }
 
 init = () => {
     waitForReady().then(() => {
         socket.subscribe(roomId);
-        sendMsg("testusr", "testusr joined the room.").then(() => loadAllMessages())
+        sendMsg("SERVER", "testusr hat den Raum betreten").then(() => {
+            loadAllMessages()
+            roomIdSpan.innerText = roomId;
+            canvas.scrollTop = canvas.scrollHeight - canvas.clientHeight;
+        });
     })
 }
 
@@ -26,46 +40,49 @@ waitForReady = async () => {
 }
 
 send = () => {
-    sendMsg("testusr", messageInput.value, null).then(() => loadAllMessages())
+    sendMsg("testusr", messageInput.value, null).then(() => loadAllMessages());
+    messageInput.value = "";
 }
 
 createMessages = () => {
-    const body = document.getElementById("chatContent")
-
-    while (body.firstChild) {
-        body.removeChild(body.firstChild);
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
     }
-
     for (let i = 0; i < messages.length; i++) {
+
         let msg = document.createDocumentFragment();
 
         const divMsg = document.createElement("div");
         divMsg.className = "message";
 
-        const divRow = document.createElement("div");
-        divRow.className = "onlyRow";
+        const username = document.createElement("span");
+        username.className = "username";
+        username.innerText = messages[i].sender + ": ";
 
-        const hUser = document.createElement("h6");
-        hUser.innerText = messages[i].sender + ": ";
+        const messageText = document.createElement("div");
+        messageText.className = "messageText";
+        messageText.innerText = messages[i].text;
 
-        const  pMsg = document.createElement("p");
-        pMsg.innerText = messages[i].text;
+        const dateDiv = document.createElement("div");
+        dateDiv.className = "date"
 
-        divRow.appendChild(hUser);
-        divRow.appendChild(pMsg);
+        const date = document.createElement("span");
+        date.innerText = messages[i].timestamp.substring(0,10).replaceAll("-", "/");
 
-        const divTime = document.createElement("div");
+        const time = document.createElement("span");
+        time.innerText = messages[i].timestamp.substring(11,16);
 
-        const pTime = document.createElement("p");
-        pTime.innerText = messages[i].timestamp.substring(11,16);
-
-        divTime.appendChild(pTime);
-
-        msg.appendChild(divMsg).appendChild(divRow);
-        divMsg.appendChild(divTime)
+        dateDiv.appendChild(time);
+        dateDiv.appendChild(date);
 
 
-        body.appendChild(msg);
+        divMsg.appendChild(username)
+        divMsg.appendChild(messageText)
+        divMsg.appendChild(dateDiv)
+
+        msg.appendChild(divMsg)
+
+        canvas.appendChild(msg);
     }
 }
 
@@ -73,10 +90,18 @@ loadAllMessages = () => {
     loadMsgs().then(msgs => {
         messages = [];
         msgs.map((mappedMsg => messages.push(mappedMsg)));
+        messages.reverse();
         createMessages();
     })
 }
 
+leave = () => {
+    sendMsg("SERVER", "testusr hat den Raum verlassen", null).then(() => {
+        socket.close();
+        window.location.href = "/Programmentwurf/";
+    });
+
+}
 //SO
 sleep = (ms) => {
     return new Promise((resolve => setTimeout(resolve, ms)));
