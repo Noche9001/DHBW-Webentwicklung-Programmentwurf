64fc27257d0003cdeeb04da57a18293d028f56a4
Noche9001 <noah.dambacher@gmx.de>
2022-11-12 13:13:38 +0100

    bugfix

--- js/components/home.js
+++ js/components/home.js
@@ -3,6 +3,19 @@ let input = document.getElementById("roomId");
 
 const body = document.getElementById("body")
 
+button.addEventListener("click", () => {
+    const roomId = input.value;
+    if(roomId.length > 0){
+        for (let i = 0; i < roomId.length; i++) {
+            if(!/([A-Za-z0-9-_])/.test(roomId.charAt(i))) {
+                alert("Sie haben ungültige Zeichen eingegeben. \n Erlaubte Zeichen: a-z, A-Z, 0-9, -, _")
+                input.value = "";
+                return;
+            }
+        }
+        window.location.href = "/Programmentwurf/room.html?roomId="+ roomId;
+    }
+})
 
 switchFunc = () => {
     let switchMode = document.getElementById("switch").innerText
